62cf7346aaf7abd49ff3e8e3cf72b2b170bbe52d
momycani <momycani>
2025-11-11 13:15:47 -0800

    Module 4 1.11

---
+++ script.js
@@ -0,0 +1,24 @@
+// template_s3chdi9
+// service_j7pu9xp
+// publicKey wsLuRZgJzGaOG0et9
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            "service_j7pu9xp",
+            "template_s3chdi9",
+           event.target,
+            "wsLuRZgJzGaOG0et9"
+        ) .then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";              
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at bethkenward3@gmail.com");
+            }); 
+}
